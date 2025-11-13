6cc5cdb7b392e825e3d52d07a354ccbaf4e2ab14
Yoares <aresmanage33@gmail.com>
2025-11-13 01:38:57 +0100

    setup files

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,13 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <string>
+
+class Zombie {
+
+};
+
+Zombie* newZombie(std::string name);
+void randomChump(std::string name);
+
+#endif // ZOMBIE_HPP
