868c339fe450d248d4079918807251620e65a811
Yasser Khoussi <ykhoussi@c4r6p3.1337.ma>
2025-11-14 19:12:41 +0100

    implement Weapon function

--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -9,7 +9,7 @@ class Weapon {
     public:
         Weapon();
         Weapon(const std::string& type);
-        const std::string getType() const;
-        void    setType(const std::string& new_val);
+        const std::string& getType() const;
+        void    setType(const std::string& new_type);
 };
 #endif
